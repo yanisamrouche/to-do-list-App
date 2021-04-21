e296a212df696ab51f260af1484f1b0afc598b5a
AMROUCHE yanis <yanis.amrouche@etu.univ-amu.fr>
2021-04-22 01:25:07 +0200

    fourth commit

--- src/App.js
+++ src/App.js
@@ -4,42 +4,53 @@ import './style.css';
 /* First component */
 function ToDoListForm(props) {
 
-  const [todo, setTodo] = useState([]);
+  var arr = props.todos;
 
   function handleChange(e){
-    setTodo(e.target.value);
+    props.setTodo(e.target.value);
   }
 
   function handleSubmit(e){
-    props.handleSubmit(todo);
-    setTodo([]);
+    props.handleSubmit(props.todo);
+    props.setTodo([]);
     e.preventDefault();
   }
 
-  function deleteSubmit(e){
-    props.deleteSubmit();
-    setTodo([]);
-    e.preventDefault();
-
+  function deleteSubmit(){
+    props.setTodos(arr.filter((el) => el.id !== props.todo.id))
+    
   }
   return (
-    <form >
-      <input type="text" placeholder="Add your task" onChange={handleChange} value={todo}></input>
+
+    <div>
+   
+      <form >
+      <input type="text" placeholder="Add your task" onChange={handleChange} value={props.todo}></input>
       <button type="submit" onClick={handleSubmit}>add</button>
       <button type="submit" onClick={deleteSubmit}>clear</button>
-    </form>
+      </form>
+
+    </div>
+    
+ 
+    
 
   );
 }
 
 /* Second component */
 function TaskList(props){
-  const arr = props.data;
-  const listItems = arr.map((val, i) => <div className="item"><li key={i}>task n° {i+1} : {val}</li>
-  <button type="submit" >remove</button>
-  </div>);
+  var arr = props.todos;
 
-  return<ul>{listItems}</ul>
+
+  const listItems = arr.map((val, i) => <div className="item"><li key={i}>task n° {i+1} : {val}</li>
+  <br></br>
+  </div>);
+  
+
+  return (<div>
+    <ul>{listItems}</ul>
+    </div>);
     
   
 
@@ -48,22 +59,23 @@ function TaskList(props){
 /* parent component */
 function DoList(props){
 
-  const [todos, setTodos] = useState(props.data);
+  const [todo, setTodo] = useState([]);
+  var [todos, setTodos] = useState(props.data);
 
   function addTodo(task){
     setTodos([...todos, task]);
   }
 
-  function deleteTodo(){
-    todos = [];
-    setTodos([todos]);
-  }
+
+
+    
+  
 
   return (
     <div className="app">
       <h1>ToDo-List</h1>
-      <ToDoListForm handleSubmit={addTodo} deleteSubmit={deleteTodo}/>
-      <TaskList data={todos}  />
+      <ToDoListForm todos={todos} todo={todo} setTodo={setTodo} handleSubmit={addTodo} />
+      <TaskList todo={todo} todos={todos} setTodo={setTodo} setTodos={setTodos} />
     </div>
 
   );
